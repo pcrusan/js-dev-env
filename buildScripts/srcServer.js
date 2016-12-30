c6e2d71385d8bc2df51f83008ed596c94292079e
Phil Crusan <Phil Crusan>
2016-12-30 03:48:16 -0500

    Fix __dirname spelling

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+var express = require ('express');
+var path = require ('path');
+var open = require ('open');
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
